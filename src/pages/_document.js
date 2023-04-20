0e678e8a6f5fb2cf18e2835ee46dd2ec5b57dc23
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-04-20 11:15:32 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <title>Média Final - IFBA</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
